5dc45778bde967d439143704f41fbb04b2f6b594
pj <pj@oc>
2026-09-16 21:29:15 +0800

    update

--- build.sh
+++ build.sh
@@ -2,16 +2,16 @@
 
 cd $(dirname $0)
 
+echo 'compile go'
+rm -rf ui/dist
+go build -ldflags="-s -w" -o certimate .
+rm -f ../artifacts/certimate
+mv certimate ../artifacts/
+
 echo 'compile web'
 cd ui
 npm install
 npm run build
 rm -f ../../artifacts/dist.tgz
 tar -czf ../../artifacts/dist.tgz ./dist
-
-echo 'compile go'
-cd ..
-go build -ldflags="-s -w" -o certimate .
-rm -f ../artifacts/certimate
-mv certimate ../artifacts/
 
